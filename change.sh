3b0b3d87389d0fc373655df3b40675769e0f0eb6
dipeshsingh253 <sinhdipesh@gmail.com>
2023-11-10 09:35:29 +0300

    moved demo content to exaple site branch

--- change.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-git filter-branch --env-filter '
-  if [ "$GIT_AUTHOR_EMAIL" = "dipeshsinh@gmail.com" ]; then
-    export GIT_AUTHOR_NAME="dipeshsingh253"
-    export GIT_AUTHOR_EMAIL="sinhdipesh@gmail.com"
-  fi
-' -- --all
-
